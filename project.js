a4368b61cdcee6853765a0a07e3dd3d8547266ce
Derek Hurley <dzhurley@gmail.com>
2020-08-22 19:53:12 -0700

    start laying out project grid

--- project.js
+++ project.js
@@ -1,14 +1,13 @@
 const setProject = (name, href, desc) => {
   const id = href.split('/').reduce((name, part) => part);
   document.querySelector('.project').innerHTML = `
-    <img src="/static/images/${id}.png" alt="${name}" />
+    <span>
+      <a class="launch" href="${href}" target="_blank" rel="noopener noreferrer">launch</a>
+      /
+      <a class="code" href="https://github.com/dzhurley/${id}" target="_blank" rel="noopener noreferrer">code</a>
+    </span>
     <p>${desc}</p>
-    <a class="launch" href="${href}" target="_blank" rel="noopener noreferrer">
-      <button>launch</button>
-    </a>
-    <a class="code" href="https://github.com/dzhurley/${id}" target="_blank" rel="noopener noreferrer">
-      <button>code</button>
-    </a>
+    <img src="/static/images/${id}.png" alt="${name}" />
   `;
 };
 
